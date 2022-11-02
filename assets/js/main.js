e64e55d10c447ee7cac0dee744f95004641253ed
MahaAmer98 <84537615+mahaamer@users.noreply.github.com>
2022-11-02 17:34:46 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+
+/*
+  Layout
+ */
+
+const swiper = new Swiper('#brands .swiper', {
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 5,
+    }
+  }
+});
+
+/*
+  Main Content
+ */
+
+/*
+  Home Page
+ */
---
+++ main.js
@@ -0,0 +1,16 @@
+// layout
+
+const swiper = new Swiper(" #brands .swiper", {
+    slidePerView: 2,
+    breakpoints: {
+        768: {
+            slidePerView: 3,
+        },
+        992: {
+            slidePerView: 5,
+        },
+    },
+});
+// Main content
+
+// Home page
